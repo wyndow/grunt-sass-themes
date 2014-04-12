8db62f06c124315f9cbdfbd4815d4705f2ecacbf
Agustin Colchado <agustin@colchado.com>
2014-04-12 10:59:49 -0700

    Adding initial gruntfile configuration

---
+++ Gruntfile.js
@@ -0,0 +1,49 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  grunt.initConfig({
+
+    clean: {
+      tmp: 'tmp'
+    },
+
+    jshint: {
+      all: [
+        'Gruntfile.js',
+        'tasks/*.js',
+        '<%= nodeunit.tests %>'
+      ],
+
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    sassThemes: {
+      main: {
+        options: {
+          output: 'tmp',
+          themes: ['black', 'white'],
+          themeDir: 'test/fixtures/themes',
+          themeImport: 'test/fixtures/theme.scss'
+        },
+        files: {
+          'simple_{{themeName}}.css': ['test/fixtures/simple.scss']
+        }
+      }
+    },
+
+    nodeunit: {
+      tests: ['test/*_test.js']
+    }
+
+  });
+
+  grunt.loadTasks('tasks');
+
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-nodeunit');
+
+  grunt.registerTask('test', ['clean', 'sassThemes', 'nodeunit']);
+};
